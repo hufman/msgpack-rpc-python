a539a68a57f8a65e5dc9aa94fc667972420bf2af
Walter Huf <hufman@gmail.com>
2013-02-02 23:37:53 -0600

    Allow the benchmark utilities to specify loop and transport

--- example/bench_client.py
+++ example/bench_client.py
@@ -1,10 +1,21 @@
+import sys
 import msgpackrpc
 import time;
 
 Num = 10000
 
+from msgpackrpc.transport import tcp as transport
+from msgpackrpc.loop import tornado as loop
+
+if '--transport' in sys.argv:
+    index = sys.argv.index('--transport')
+    transport = __import__("msgpackrpc.transport.%s"%sys.argv[index+1], fromlist=["msgpackrpc.transport"])
+if '--loop' in sys.argv:
+    index = sys.argv.index('--loop')
+    loop = __import__("msgpackrpc.loop.%sloop"%sys.argv[index+1], fromlist=["msgpackrpc.loop"])
+
 def run_call():
-    client = msgpackrpc.Client(msgpackrpc.Address("localhost", 18800))
+    client = msgpackrpc.Client(msgpackrpc.Address("localhost", 18800), loop=loop.Loop(), builder=transport)
     before = time.time()
     for x in range(Num):
         client.call('sum', 1, 2)
@@ -14,7 +25,7 @@ def run_call():
     print("call: {0} qps".format(Num / diff))
 
 def run_call_async():
-    client = msgpackrpc.Client(msgpackrpc.Address("localhost", 18800))
+    client = msgpackrpc.Client(msgpackrpc.Address("localhost", 18800), loop=loop.Loop(), builder=transport)
     before = time.time()
     for x in range(Num):
         # TODO: replace with more heavy sample
@@ -26,7 +37,7 @@ def run_call_async():
     print("async: {0} qps".format(Num / diff))
 
 def run_notify():
-    client = msgpackrpc.Client(msgpackrpc.Address("localhost", 18800))
+    client = msgpackrpc.Client(msgpackrpc.Address("localhost", 18800), loop=loop.Loop(), builder=transport)
     before = time.time()
     for x in range(Num):
         client.notify('sum', 1, 2)
--- example/bench_server.py
+++ example/bench_server.py
@@ -1,9 +1,21 @@
+import sys
 import msgpackrpc
+
+from msgpackrpc.transport import tcp as transport
+from msgpackrpc.loop import tornado as loop
+
+if '--transport' in sys.argv:
+    index = sys.argv.index('--transport')
+    transport = __import__("msgpackrpc.transport.%s"%sys.argv[index+1], fromlist=["msgpackrpc.transport"])
+
+if '--loop' in sys.argv:
+    index = sys.argv.index('--loop')
+    loop = __import__("msgpackrpc.loop.%sloop"%sys.argv[index+1], fromlist=["msgpackrpc.loop"])
 
 class SumServer(object):
     def sum(self, x, y):
         return x + y
 
-server = msgpackrpc.Server(SumServer())
+server = msgpackrpc.Server(SumServer(), loop=loop.Loop(), builder=transport)
 server.listen(msgpackrpc.Address("localhost", 18800))
 server.start()
